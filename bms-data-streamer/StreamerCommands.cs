bfef85c06d0902f294658505edf796870a7e9812
Ayan-Liaquath <105066915+Ayan-Liaquath@users.noreply.github.com>
2022-09-10 18:28:56 +0530

    Create StreamerCommands.cs

---
+++ bms-data-streamer/StreamerCommands.cs
@@ -0,0 +1,9 @@
+namespace bms_data_streamer
+{
+    using System.Collections.Generic;
+
+    public static class StreamerCommands
+    {
+        public static IList<string> Commands => new List<string> { "sender-executable" };
+    }
+}
